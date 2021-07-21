ccf6d7f441ae441a7d71106b683811a500de7a18
leeee36 <si859517@gmail.com>
2021-07-21 19:24:55 +0900

    first commit

---
+++ 2.variables+datatypes/bool_datatypes.py
@@ -0,0 +1,10 @@
+# bool 자료형
+# true false
+a = True
+b = False
+print(type(a), type(b))
+
+
+x = 1
+y = 2
+print(x > y)
---
+++ 2.variables+datatypes/number_datatypes.py
@@ -0,0 +1,23 @@
+#정수형
+a = 153
+print(type(a))
+a = 0
+print(type(a))
+a = -123
+print(type(a))
+
+
+#실수형
+b = 123.12
+print(type(b))
+b = 1.2
+print(type(b))
+
+
+#복소수형
+c = 2 - 3j
+print(type(c))
+print(c.real)
+print(c.imag)
+print(c.conjugate())
+print(abs(c))
