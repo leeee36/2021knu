8b0e8635c8972fb4f6e8a7996329e2a521e53380
leeee36 <si859517@gmail.com>
2021-07-21 20:05:14 +0900

    first commit

---
+++ 1.assignment/week1_assignment.py
@@ -0,0 +1,7 @@
+name = '이상욱'
+major = '수학과'
+grade = '1학년'
+goal = '파이썬 초급반의 수업을 성실히 이수하여 파이썬의 기초를 다지겠습니다.'
+new_goal = f'{goal}'
+print('경북대학교 {0} {1} {2}은 이번 sw융합교육을 통해'.format(major, grade, name))
+print(new_goal)
